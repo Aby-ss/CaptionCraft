e34270c8f51b4cca84586f6b21222cfe4a4b3133
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-06-22 20:10:25 +0400

    Fixed Return error ✅

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import time
-from numpy as numpy
+import numpy as np
 
 from rich import box
 from rich import print
@@ -17,15 +17,17 @@ def caption_generations():
 
     with Progress() as progress:
 
-    task1 = progress.add_task("[red]Downloading...", total=100)
-    task2 = progress.add_task("[yellow]Processing...", total=100)
-    task3 = progress.add_task("[green]Writing...", total=100)
+        task1 = progress.add_task("[red]Downloading...", total=100)
+        task2 = progress.add_task("[yellow]Processing...", total=100)
+        task3 = progress.add_task("[green]Writing...", total=100)
 
-    while not progress.finished:
-        progress.update(task1, advance=0.5)
-        progress.update(task2, advance=0.4)
-        progress.update(task3, advance=0.7)
-        time.sleep(0.02)
+        while not progress.finished:
+            progress.update(task1, advance=0.5)
+            progress.update(task2, advance=0.4)
+            progress.update(task3, advance=0.7)
+            time.sleep(0.02)
 
     caption_prompt_panel = Panel(f"{caption_prompt}", box = box.SQUARE, border_style="bold white")
-    return caption_prompt_panel
+    print(caption_prompt_panel)
+
+caption_generations()
